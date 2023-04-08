e89c892f5f8c011ddfcd9e2958fc28342dcedc89
Mr Killer 1109 <69214079+harshil8981@users.noreply.github.com>
2023-04-08 13:28:01 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -6,7 +6,7 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("Mrkiller Bᴏᴛᴢ")
+    return web.json_response("Mrkiller Bot")
 
 async def web_server():
     web_app = web.Application(client_max_size=30000000)
