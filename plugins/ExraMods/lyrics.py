001c7e1ab944bbcfaf968e3efc68a7480df57bf9
Mr Killer 1109 <69214079+harshil8981@users.noreply.github.com>
2023-04-08 13:02:09 +0530

    Add files via upload

---
+++ plugins/ExraMods/lyrics.py
@@ -0,0 +1,41 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton 
+
+import requests 
+
+import os
+
+
+API = "https://apis.xditya.me/lyrics?song="
+
+@Client.on_message(filters.text & filters.command(["lyrics"]))
+async def sng(bot, message):
+        if not message.reply_to_message:
+          await message.reply_text("Please reply to a message")
+        else:          
+          mee = await message.reply_text("`Searching 🔎`")
+          song = message.reply_to_message.text
+          chat_id = message.from_user.id
+          rpl = lyrics(song)
+          await mee.delete()
+          try:
+            await mee.delete()
+            await bot.send_message(chat_id, text = rpl, reply_to_message_id = message.id, reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇs ", url = f"t.me/Hpbot_update")]]))
+          except Exception as e:                            
+             await message.reply_text(f"I Can't Find A Song With `{song}`", quote = True, reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇs", url = f"t.me/Hpbot_update")]]))
+
+
+def search(song):
+        r = requests.get(API + song)
+        find = r.json()
+        return find
+       
+def lyrics(song):
+        fin = search(song)
+        text = f'**🎶 Sᴜᴄᴄᴇꜱꜰᴜʟʟy Exᴛʀᴀᴄᴛᴇᴅ Lyɪʀɪᴄꜱ Oꜰ {song}**\n\n'
+        text += f'`{fin["lyrics"]}`'
+        text += '\n\n\n**Made By Artificial Intelligence**'
+        return text
+
+
+
